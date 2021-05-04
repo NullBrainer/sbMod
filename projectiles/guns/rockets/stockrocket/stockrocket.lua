deb54a177beeaf9aab304e161aeddd735b3600a0
NullBrainer <ed.r1331@yahoo.com>
2021-05-04 13:28:23 -0700

    Changed heal directory to medibeamheal and all healing files to medibeamheal.

---
+++ projectiles/guns/rockets/stockrocket/stockrocket.lua
@@ -0,0 +1,9 @@
+require "/scripts/vec2.lua"
+
+function init()
+  self.power = config.getParameter("power")
+end
+
+function update(dt)
+-- projectiles have decreasing power over time from being initialized
+end
